39ed536ade6df19da2196d4f16140c281a09839e
Jules010209 <67187964+Jules010209@users.noreply.github.com>
2022-08-16 19:10:45 +0200

    Add files via upload

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <header>
+          <nav>
+            <p align="center">
+              Navigation
+            </p>
+            {/* <br></br> */}
+            <ul>
+              <li>
+                <a href='/'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='/about'>
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <section>
+          <div className='articles'>
+            <a href='https://blogs-articles.com/posts/1024252?query=other'>
+            <div className='art-1'>
+              <div>
+                <h4>How many website was created ?</h4>
+                <p>This article was wrote by Jules</p>
+              </div>
+              <div>
+                <img alt='No' src='https://oxie.fr/assets/images/favicon.ico'></img>
+              </div>
+              <hr color='black'></hr>
+              <p>Published at 18:34</p>
+            </div>
+            </a>
+          </div>
+        </section>
+      </div>
   );
 }
 
