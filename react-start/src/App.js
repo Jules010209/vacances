485acf8fd5d0d2a91072d19e6778ed90a8f36049
Jules010209 <67187964+Jules010209@users.noreply.github.com>
2022-09-07 19:25:06 +0200

    Add files via upload

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,47 +1,12 @@
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-      <div>
-        <header>
-          <nav>
-            <p align="center">
-              Navigation
-            </p>
-            {/* <br></br> */}
-            <ul>
-              <li>
-                <a href='/'>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href='/about'>
-                  About
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-
-        <section>
-          <div className='articles'>
-            <a href='https://blogs-articles.com/posts/1024252?query=other'>
-            <div className='art-1'>
-              <div>
-                <h4>How many website was created ?</h4>
-                <p>This article was wrote by Jules</p>
-              </div>
-              <div>
-                <img alt='No' src='https://oxie.fr/assets/images/favicon.ico'></img>
-              </div>
-              <hr color='black'></hr>
-              <p>Published at 18:34</p>
-            </div>
-            </a>
-          </div>
-        </section>
-      </div>
+    <div className="App">
+      <Navbar/>
+    </div>
   );
 }
 
